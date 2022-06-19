73ab4eee244e14ff18e19e1ee9e68efc7e08a4a7
644708020011 <107331159+644708020011@users.noreply.github.com>
2022-06-19 09:36:34 +0700

    week2 person del.py

--- person2.py
+++ person2.py
@@ -11,17 +11,22 @@ class Person:
     def show(self):
         print(f'Name:{self.name} Gender:{self.gender} Profession: {self.profession} study: {self.study}')
 
-#person 1
-jessa = Person('Jessa' , 'Female' , 'software Engineer', 0 )
-jessa.work()
-jessa.show()
+    def __del__(self):
+        print("Object was destory")
 
-#person 2
-jon = Person('Jon' , 'Male' , 'Doctor' , 15 )
-jon.work()
-jon.show()
 
-#person
-lisa = Person('Lalisa' , 'FEmale' , 'Koren Singer' )
-lisa.work()
-lisa.show()
+if __name__ == "__main__":
+    #person 1
+    jessa = Person('Jessa' , 'Female' , 'software Engineer', 0 )
+    jessa.work()
+    jessa.show()
+
+    #person 2
+    jon = Person('Jon' , 'Male' , 'Doctor' , 15 )
+    jon.work()
+    jon.show()
+
+    #person
+    lisa = Person('Lalisa' , 'Female' , 'Koren Singer', 0 )
+    lisa.work()
+    lisa.show()
