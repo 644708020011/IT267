ff59a8c60c44209a7ea9a3cac6cf08c05a9eb9a7
644708020011 <107331159+644708020011@users.noreply.github.com>
2022-06-26 09:53:23 +0700

    my_math.py

---
+++ Modulepackage/my_math_use.py
@@ -0,0 +1,13 @@
+# วิธีที่ 1
+print('**** วิธีที่ 1 ****')
+from my_math import sqrt,circle_area
+
+print(f'sqrt of 5 = {sqrt (5)}')
+print(f'circle_area = {circle_area(2):,.2f}')
+
+#step 2
+import my_math
+
+print('**** วิธีที่ 2 ****')
+print(f'sqrt of 5 = {sqrt (5)}')
+print(f'circle_area = {circle_area(2):,.2f}')
