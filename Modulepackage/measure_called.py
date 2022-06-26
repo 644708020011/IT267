bd5ad2b2e7d78586333a61e95ee1d9ead73579ee
644708020011 <107331159+644708020011@users.noreply.github.com>
2022-06-26 10:47:37 +0700

    measure.py

---
+++ Modulepackage/measure_called.py
@@ -0,0 +1,18 @@
+# วิธีการที้ 1
+from msilib import typemask
+from measure import Measure
+
+if __name__ == '__main__':
+    mobj = Measure()
+    #print (f'5 cm = {mobj.cm_inch(5):.2f} inch')
+    #print (f'18.5 inch = {mobj.inch_cm(18.5):.2f} cm')
+    typem = input('choose type (1: inch, 2: cm): ')
+
+    number = float(input('Enter number: '))
+
+    if typem == '1':
+        print (f'(number)inch = {mobj.inch_cm(number):.2f} cm')
+    elif typem == '2':
+        print (f'(number) cm = {mobj.cm_inch(number):.2f} inch')
+    else:
+        print('choose wrong type')
